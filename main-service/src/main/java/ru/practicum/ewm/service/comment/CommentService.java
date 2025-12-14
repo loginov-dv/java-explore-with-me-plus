74cc08b70c46a7297e86b75cc879523eddcb87c7
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-12-14 11:50:09 +0300

    feat(main-service): добавить сервис и имплементацию для комментариев

---
+++ main-service/src/main/java/ru/practicum/ewm/service/comment/CommentService.java
@@ -0,0 +1,20 @@
+package ru.practicum.ewm.service.comment;
+
+import ru.practicum.ewm.dto.comment.CommentDto;
+import ru.practicum.ewm.dto.comment.CommentParam;
+import ru.practicum.ewm.dto.comment.UpdateCommentRequest;
+
+import java.util.List;
+
+public interface CommentService {
+
+    CommentDto create(CommentParam commentParam);
+
+    CommentDto update(CommentParam commentParam);
+
+    void deleteByUser(CommentParam commentParam);
+
+    void deleteByAdmin(Long eventId, Long commentId);
+
+    List<CommentDto> findAllByEventId(Long eventId);
+}
---
+++ main-service/src/main/java/ru/practicum/ewm/service/comment/CommentServiceImpl.java
@@ -0,0 +1,167 @@
+package ru.practicum.ewm.service.comment;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.ewm.dto.comment.CommentDto;
+import ru.practicum.ewm.dto.comment.CommentParam;
+import ru.practicum.ewm.dto.comment.UpdateCommentRequest;
+import ru.practicum.ewm.event.EventRepository;
+import ru.practicum.ewm.exception.AccessViolationException;
+import ru.practicum.ewm.exception.NotFoundException;
+import ru.practicum.ewm.mapper.CommentMapper;
+import ru.practicum.ewm.model.comment.Comment;
+import ru.practicum.ewm.model.event.Event;
+import ru.practicum.ewm.model.user.User;
+import ru.practicum.ewm.repository.UserRepository;
+import ru.practicum.ewm.repository.comment.CommentRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+public class CommentServiceImpl implements CommentService {
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final EventRepository eventRepository;
+
+    @Override
+    public CommentDto create(CommentParam commentParam) {
+        log.debug("Comment create request for eventId = {} by userId = {}: {}",
+                commentParam.getEventId(), commentParam.getUserId(), commentParam.getCommentDto());
+
+        Optional<User> maybeUser = userRepository.findById(commentParam.getUserId());
+
+        if (maybeUser.isEmpty()) {
+            log.warn("User with id = {} not found", commentParam.getUserId());
+            throw new NotFoundException(String.format("User with id = %d not found", commentParam.getUserId()));
+        }
+
+        Optional<Event> maybeEvent = eventRepository.findById(commentParam.getEventId());
+
+        if (maybeEvent.isEmpty()) {
+            log.warn("Event with id = {} not found", commentParam.getEventId());
+            throw new NotFoundException(String.format("Event with id = %d not found", commentParam.getEventId()));
+        }
+
+        Comment comment = commentRepository.save(CommentMapper.toNewComment(maybeUser.get(),
+                maybeEvent.get(), commentParam.getCommentDto()));
+
+        log.info("New comment added: {}", comment);
+        return CommentMapper.toCommentDto(comment);
+    }
+
+    @Override
+    public CommentDto update(CommentParam commentParam) {
+        log.debug("CommentId = {} update request for eventId = {} by userId = {}: {}",
+                commentParam.getCommentId(), commentParam.getEventId(), commentParam.getUserId(),
+                commentParam.getUpdateCommentRequest());
+
+        Optional<User> maybeUser = userRepository.findById(commentParam.getUserId());
+
+        if (maybeUser.isEmpty()) {
+            log.warn("User with id = {} not found", commentParam.getUserId());
+            throw new NotFoundException(String.format("User with id = %d not found", commentParam.getUserId()));
+        }
+
+        if (!eventRepository.existsById(commentParam.getEventId())) {
+            log.warn("Event with id = {} not found", commentParam.getEventId());
+            throw new NotFoundException(String.format("Event with id = %d not found", commentParam.getEventId()));
+        }
+
+        Optional<Comment> maybeComment = commentRepository.findById(commentParam.getCommentId());
+
+        if (maybeComment.isEmpty()) {
+            log.warn("Comment with id = {} not found", commentParam.getEventId());
+            throw new NotFoundException(String.format("Comment with id = %d not found", commentParam.getEventId()));
+        }
+
+        Comment comment = maybeComment.get();
+        log.debug("Initial comment state: {}", comment);
+
+        if (!comment.getUser().getId().equals(commentParam.getUserId())) {
+            log.warn("No access to edit comment");
+            throw new AccessViolationException("No access to edit comment");
+        }
+
+        CommentMapper.updateFields(comment, commentParam.getUpdateCommentRequest());
+        comment = commentRepository.save(comment);
+
+        log.debug("Comment has been updated: {}", comment);
+        return CommentMapper.toCommentDto(comment);
+    }
+
+    @Override
+    public void deleteByUser(CommentParam commentParam) {
+        log.debug("CommentId = {} delete request for eventId = {} by userId = {}",
+                commentParam.getCommentId(), commentParam.getEventId(), commentParam.getUserId());
+
+        if (!userRepository.existsById(commentParam.getUserId())) {
+            log.warn("User with id = {} not found", commentParam.getUserId());
+            throw new NotFoundException(String.format("User with id = %d not found", commentParam.getUserId()));
+        }
+
+        if (!eventRepository.existsById(commentParam.getEventId())) {
+            log.warn("Event with id = {} not found", commentParam.getEventId());
+            throw new NotFoundException(String.format("Event with id = %d not found", commentParam.getEventId()));
+        }
+
+        Optional<Comment> maybeComment = commentRepository.findById(commentParam.getCommentId());
+
+        if (maybeComment.isEmpty()) {
+            log.warn("Comment with id = {} not found", commentParam.getCommentId());
+            throw new NotFoundException(String.format("Comment with id = %d not found", commentParam.getCommentId()));
+        }
+
+        Comment comment = maybeComment.get();
+
+        if (!comment.getUser().getId().equals(commentParam.getUserId())) {
+            log.warn("No access to delete comment");
+            throw new AccessViolationException("No access to delete comment");
+        }
+
+        commentRepository.deleteById(commentParam.getCommentId());
+        log.debug("Comment has been deleted by user");
+    }
+
+    @Override
+    public void deleteByAdmin(Long eventId, Long commentId) {
+        log.debug("Comment id = {} delete request for eventId = {} by admin", commentId, eventId);
+
+        if (!eventRepository.existsById(eventId)) {
+            log.warn("Event with id = {} not found", eventId);
+            throw new NotFoundException(String.format("Event with id = %d not found", eventId));
+        }
+
+        Optional<Comment> maybeComment = commentRepository.findById(commentId);
+
+        if (maybeComment.isEmpty()) {
+            log.warn("Comment with id = {} not found", eventId);
+            throw new NotFoundException(String.format("Comment with id = %d not found", eventId));
+        }
+
+        commentRepository.deleteById(commentId);
+        log.debug("Comment has been deleted by admin");
+    }
+
+    @Override
+    public List<CommentDto> findAllByEventId(Long eventId) {
+        log.debug("Comments request for eventId = {}", eventId);
+
+        if (!eventRepository.existsById(eventId)) {
+            log.warn("Event with id = {} not found", eventId);
+            throw new NotFoundException(String.format("Event with id = %d not found", eventId));
+        }
+
+        List<Comment> comments = commentRepository.findByEventId(eventId);
+        log.debug("comments.size = {}", comments.size());
+
+        return comments.stream()
+                .map(CommentMapper::toCommentDto)
+                .toList();
+    }
+}
